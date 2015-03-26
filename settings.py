b15bda4bd174ac48a1b0a2c03d8c64d77bca6269
Tech <techministry@fellowshipmissionchurch.org>
2015-03-26 01:11:13 -0400

    settings for heroku functionality

---
+++ settings.py
@@ -0,0 +1,36 @@
+PLIVO_AUTH_ID = "MAMTDMMJZKMJQ0YJRMMW"
+PLIVO_TOKEN = "Yjg0M2RiOGViZTlkNmU2ZDI1NzcxMGE0YmM3MTIw"
+
+from_number = "16176583908"
+to_number = "16178070291"
+admins = ['16178070291','13392366863'] #,'18576157065','16176820140']
+
+#num_local = ""
+#num_formal = ""
+
+#Prayer coordinator PIN to host the prayer line
+pin_number = '8824'
+
+CONFERENCE_UNMUTE_SEQUENCE = '9'
+
+
+#NOTIFY_ADMIN = ['16177721818','16178070291','13392366863']
+NOTIFY_ADMIN = {
+				'16177721818':'Bro. Kevin',
+				'16178070291':'Bro. Kevin',
+				'13392366863':'Bro. Kevin',
+				'16176820140':'Sis. Venice',
+				'18572668104':'Sis. Venice',
+				'18576157065':'Sis. Edwards',
+				'18572257827':'Sis. Lee',
+				'16175719666':'Deac. Damian',
+				'19788366055':'Deac. Dea',
+				'16178034647':'Minister Leon',
+				'16176025907':'Minister McKoy',
+				'17744447606':'Deacon Pires',
+				'17814925617':'Deacon Quinones',
+				'18574174166':'Minister Rogers',
+				'16175493574':'Bro. Powell',
+				'18572465618':'Sis. Moses',
+				'16179971970':'Sis. Damian'
+				}
