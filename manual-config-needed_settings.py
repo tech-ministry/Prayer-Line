cdcefe3016bfe3928f8154f71033cb2251061669
Tech <techministry@fellowshipmissionchurch.org>
2015-04-06 23:22:09 -0400

    edit settings and resave as settings.py

---
+++ manual-config-needed_settings.py
@@ -0,0 +1,31 @@
+PLIVO_AUTH_ID = "xxxxxxxxxxxxxxxxxx"
+PLIVO_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+from_number = "xxxxxxxxxxx"
+to_number = "xxxxxxxxxxx"
+admins = ['xxxxxxxxxxx','xxxxxxxxxxx']
+
+#Prayer coordinator PIN to host the prayer line
+pin_number = 'xxxx'
+
+CONFERENCE_UNMUTE_SEQUENCE = '9'
+
+NOTIFY_ADMIN = {
+				'xxxxxxxxxxx':'Bro. ',
+				'xxxxxxxxxxx':'Bro. ',
+				'xxxxxxxxxxx':'Bro. ',
+				'xxxxxxxxxxx':'Sis. ',
+				'xxxxxxxxxxx':'Sis. ',
+				'xxxxxxxxxxx':'Sis. ',
+				'xxxxxxxxxxx':'Sis. ',
+				'xxxxxxxxxxx':'Deac. ',
+				'xxxxxxxxxxx':'Deac. ',
+				'xxxxxxxxxxx':'Minister ',
+				'xxxxxxxxxxx':'Minister ',
+				'xxxxxxxxxxx':'Deacon ',
+				'xxxxxxxxxxx':'Deacon ',
+				'xxxxxxxxxxx':'Minister ',
+				'xxxxxxxxxxx':'Bro. ',
+				'xxxxxxxxxxx':'Sis. ',
+				'xxxxxxxxxxx':'Pastor '
+				}
